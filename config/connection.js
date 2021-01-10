e361c9b9157a24eb0acf6159804ee46e740346dc
sahu <sahu4264@gmail.com>
2021-01-10 15:42:29 +0300

    final

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,9 @@ const state={
 }
 module.exports.connect=function(done){
     /*const url='mongodb://localhost:27017'*/
-   // const url = "mongodb+srv://shahana:eshanali@skmcluster.spxff.mongodb.net/test"
+//    const url = "mongodb+srv://shahana:eshanali@skmcluster.spxff.mongodb.net/test"
     var url           =    process.env.MONGODB_URL;
-    //const url='mongodb+srv://shahanafaisal:eshan@newcluster.v2pix.mongodb.net/test?authSource=admin&replicaSet=atlas-10y7ti-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+   // const url='mongodb+srv://shahanafaisal:eshan@newcluster.v2pix.mongodb.net/test?authSource=admin&replicaSet=atlas-10y7ti-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
     const dbname='fooddelivery'
 
     mongoClient.connect(url,(err,data)=>{
